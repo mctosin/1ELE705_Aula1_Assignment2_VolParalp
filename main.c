14fc435b46765f5632b518a5f051e7a244814692
mctosin <mctosin@gmail.com>
2021-01-30 15:00:30 -0300

    Template concluído

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 int main() {
 	float a, b, c, s;
 	fprintf(stderr, "Entre com as variaveis a b c: Ex 4 5.0 6.20 <enter> \n");
-	scanf("%f%f%f", &a, &b, &c);
-	s = a * b * c;
-	printf("%.2f\n", s);
+	
+	// Não modifique o código acima.
+
+	// Insira seu código aqui
+
 	return 0;
 }
